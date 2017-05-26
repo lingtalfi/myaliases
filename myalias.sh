1d121edb48e95dd9027d5ddd5452a04ca8d7f3a9
lingtalfi <lingtalfi@gmail.com>
2017-05-26 18:02:30 +0200

    update

--- myalias.sh
+++ myalias.sh
@@ -89,7 +89,7 @@ alias llog='tail -f -n 100 /myphp/leaderfit/leaderfit/logs/kamille.log.txt'
 
 alias xiaoo='php -f /myphp/leaderfit/leaderfit/scripts/xiao-generator.php'
 alias prcc='php -f /myphp/leaderfit/leaderfit/scripts/generator.php'
-
+alias cca='rm -r /myphp/leaderfit/leaderfit/cache'
 
 
 #------------------------------
