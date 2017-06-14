0953add1ca93052ac454b48153e6a51754156a38
lingtalfi <lingtalfi@gmail.com>
2017-06-14 06:12:46 +0200

    update

--- myalias.sh
+++ myalias.sh
@@ -93,6 +93,7 @@ alias xiaoo='php -f /myphp/leaderfit/leaderfit/scripts/xiao-generator.php'
 alias xiaop='php -f /myphp/leaderfit/leaderfit/scripts/xiao-generator-peipei.php'
 alias prcc='php -f /myphp/leaderfit/leaderfit/scripts/generator.php'
 alias cca='rm -r /myphp/leaderfit/leaderfit/cache'
+alias ksql='/Applications/MAMP/Library/bin/mysql --user=root --password=root --database=kamille --execute="DROP DATABASE if exists kamille; CREATE DATABASE kamille character set utf8 collate utf8_general_ci;"; /Applications/MAMP/Library/bin/mysql -u root -proot kamille < /myphp/leaderfit/leaderfit/class-modules/Ekom/assets/db/ekom-kamille.sql; /Applications/MAMP/Library/bin/mysql -u root -proot kamille < /myphp/leaderfit/leaderfit/class-modules/PeiPei/assets/db/peipei.sql;'
 
 
 #------------------------------
