a00262ff6551815ec6b97f80954fe4c2b4ed30b3
lingtalfi <lingtalfi@gmail.com>
2018-04-03 07:18:33 +0200

    update

--- ling-root-alias.sh
+++ ling-root-alias.sh
@@ -1,3 +1,7 @@
-# add this at the end of your root's .bashrc
+# to install: do this as root: 
+# mkdir -p /root/tools; cd /root/tools; wget https://github.com/lingtalfi/myaliases/blob/master/ling-root-alias.sh; chmod u+x ling-root-alias.sh
+
+alias aa='vim /root/tools/ling-root-aliases.sh'
+alias aas='source /root/tools/ling-root-aliases.sh'
 alias ar='service apache2 reload'
 alias alog='tail -f -n100 /var/log/apache2/ling/all.log'
